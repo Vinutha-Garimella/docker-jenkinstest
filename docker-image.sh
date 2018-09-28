dcf7f618c738def9bedd4f9c33d05259eea5c7b5
vinutha96 <vinutha.shanmukha@gmail.com>
2018-09-28 11:09:42 +0530

    remove echo statemnets to the commands

--- docker-image.sh
+++ docker-image.sh
@@ -1,5 +1,5 @@
 #create a docker image
-echo 'dokcer image build -t demo .'
+dokcer image build -t demo .
 
 # create a docker image
-echo 'docker run -it -p 3001:3001 -d --name demo-test demo'
+docker run -it -p 3001:3001 -d --name demo-test demo
