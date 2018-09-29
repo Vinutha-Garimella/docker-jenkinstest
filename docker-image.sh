6af376a48f806d28e50fbe57b7cb5b41c6e695f9
vinutha96 <vinutha.shanmukha@gmail.com>
2018-09-29 13:18:12 +0530

    add tag to the image

--- docker-image.sh
+++ docker-image.sh
@@ -1,4 +1,10 @@
 #create a docker image
 #docker image build -t demo .
 
+# create a dcoker image 
 docker image build -t demo1 .
+
+# create a tag for the built image 
+docker tag demo1 registry.hotelsoft.com/demo1
+
+# push image through curl command 
