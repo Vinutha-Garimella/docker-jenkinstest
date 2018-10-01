890bd45039abda1dd8393fa3372ee97ae09a9383
vinutha96 <vinutha.shanmukha@gmail.com>
2018-10-01 15:22:58 +0530

    docker demo2 image

--- docker-image.sh
+++ docker-image.sh
@@ -1,11 +1,8 @@
-#create a docker image
-#docker image build -t demo .
-
 # create a dcoker image 
-docker image build -t demo1 .
+docker image build -t demo2 .
 
 # create a tag for the built image 
-docker tag demo1 registry.hotelsoft.com/demo1
+docker tag demo1 registry.hotelsoft.com/demo2
 
 # push image to the registry(the tag which you created for the image)
-docker push registry.hotelsoft.com/demo1
+docker push registry.hotelsoft.com/demo2
